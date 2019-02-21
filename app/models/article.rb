b6a00f67418eeb69dfab12d7ba2f596f37ee6862
India Unger-Harquail <ieu@andrew.cmu.edu>
2019-02-21 17:52:17 -0500

    built articles model and added relationships

---
+++ app/models/article.rb
@@ -0,0 +1,9 @@
+class Article < ApplicationRecord
+  has_one :category
+
+  validates_presence_of :title
+  validates_presence_of :content
+
+  scope :alphabetical, -> { order('title') }
+  scope :active, -> { where('active = ?', true) }
+end
